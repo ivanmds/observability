ab22e552c17308076f57028b3389d3d91ea7d3ff
Ivan Meireles da Silva <isilva@bankly.com.br>
2022-08-12 21:29:30 -0300

    begin

---
+++ customer/customer.api/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using customer.api.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace customer.api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private static List<Customer> _customers = new List<Customer>();
+        private readonly ILogger<CustomerController> _logger;
+
+
+        public CustomerController(ILogger<CustomerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return _customers;
+        }
+
+        [HttpPut]
+        public void Put(Customer customer)
+        {
+            _customers.Add(customer);
+            var jsonLog = JsonConvert.SerializeObject(customer);
+            _logger.LogInformation(jsonLog);
+        }
+    }
+}
---
+++ customer/customer.api/Model/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace customer.api.Model
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string MotherName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
